5de519d71d13f0080aa08952d58bd75d82e95faa
Puzhe Li <609788969@qq.com>
2024-06-01 16:40:32 +0800

    adjust thr and make table in find_spike_events2.ipynb

--- dataset.py
+++ dataset.py
@@ -1,4 +1,20 @@
-path = {
-    'raw': r'D:\Data\as_monkeys\raw\X8_data',
-    'tmp': r'D:\Data\as_monkeys\tmp'
-}
+import os.path
+
+import pandas as pd
+
+path = {'root': r'D:\Data\as_monkeys'}
+
+path['raw'] = os.path.join(path['root'], 'raw', 'X8_data')
+path['tmp'] = os.path.join(path['root'], 'tmp')
+path['results'] = os.path.join(path['root'], 'results')
+
+for folder in path.values():
+    if folder == 'root':
+        continue
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+def load_info():
+    return pd.read_excel(os.path.join(path['root'], 'monkeys.xlsx'), index_col=0)
