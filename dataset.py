1f3e56f04d75a1fb699c40a34ff3da7cbe5c9311
Puzhe Li <609788969@qq.com>
2024-08-01 13:10:48 +0800

    init dataset for analysis on 2024-08-01

--- dataset.py
+++ dataset.py
@@ -3,10 +3,11 @@ import os.path
 import pandas as pd
 
 path = {'root': r'D:\Data\as_monkeys'}
-
-path['raw'] = os.path.join(path['root'], 'raw', 'X8_data')
-path['tmp'] = os.path.join(path['root'], 'tmp')
-path['results'] = os.path.join(path['root'], 'results')
+path['free_eeg'] = os.path.join(path['root'], 'free_eeg')
+path['raw'] = os.path.join(path['free_eeg'], 'raw')
+path['tmp'] = os.path.join(path['free_eeg'], 'tmp')
+path['tbl'] = os.path.join(path['free_eeg'], 'tbl')
+path['fig'] = os.path.join(path['free_eeg'], 'fig')
 
 for folder in path.values():
     if folder == 'root':
--- tmp/sort_files.py
+++ tmp/sort_files.py
@@ -3,7 +3,8 @@ import os.path
 
 import dataset
 
-file_list = glob.glob(os.path.join(dataset.path['raw'], '**', '**.edf'), recursive=True)
+path_sort = r'F:\free_eeg\raw'
+file_list = glob.glob(os.path.join(path_sort, '**', '**.fif'), recursive=True)
 for file in file_list:
     if not file.endswith('merge.edf'):
         print(file)
